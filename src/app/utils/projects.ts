294dcd6bea10df7c6d324c67da041217cf4c5c0f
avsg1991@gmail.com <avsg1991@gmail.com>
2024-09-03 15:28:04 -0400

    PP-3: fix assets images path

--- src/app/utils/projects.ts
+++ src/app/utils/projects.ts
@@ -4,10 +4,10 @@ export const projects: projectI[] = [
     description: 'Es una herramienta digital diseñada para simplificar la gestión de negocio desde un solo lugar y al instante.',
     color: '#7269ff',
     images: [
-      '/assets/images/portfolio/taclia-logo.png',
-      '/assets/images/portfolio/taclia/taclia-calendario.png',
-      '/assets/images/portfolio/taclia/taclia-control-horario.png',
-      '/assets/images/portfolio/taclia/taclia-mobile.png'
+      './assets/images/portfolio/taclia-logo.png',
+      './assets/images/portfolio/taclia/taclia-calendario.png',
+      './assets/images/portfolio/taclia/taclia-control-horario.png',
+      './assets/images/portfolio/taclia/taclia-mobile.png'
     ],
     technologies: [
       'devicon-javascript-plain',
@@ -24,10 +24,10 @@ export const projects: projectI[] = [
     description: 'Es un sistema de agendamiento online para IPS, Centros Médicos, Clínicas, Hospitales y Torres Médicas, que permite que el paciente autogestione sus citas a través de la web.',
     color: '#63cbb2',
     images: [
-      '/assets/images/portfolio/dondoctor-logo.png',
-      '/assets/images/portfolio/dondoctor/dondoctor-agendador-1.png',
-      '/assets/images/portfolio/dondoctor/dondoctor-agendador-2.png',
-      '/assets/images/portfolio/dondoctor/dondoctor-agendador-3.png'
+      './assets/images/portfolio/dondoctor-logo.png',
+      './assets/images/portfolio/dondoctor/dondoctor-agendador-1.png',
+      './assets/images/portfolio/dondoctor/dondoctor-agendador-2.png',
+      './assets/images/portfolio/dondoctor/dondoctor-agendador-3.png'
     ],
     technologies: [
       'devicon-typescript-plain',
@@ -45,8 +45,8 @@ export const projects: projectI[] = [
     description: 'Una aplicación desarrollada por un equipo de venezolanos, encontró una forma particular de que las personas se relacionaran: a través de videos en los que los usuarios indican sus gustos, mediante palabras claves, en una pieza de 15 segundos.',
     color: '#ff6969',
     images: [
-      '/assets/images/portfolio/playfan-logo.png',
-      '/assets/images/portfolio/playfan/playfan-flow.jpg'
+      './assets/images/portfolio/playfan-logo.png',
+      './assets/images/portfolio/playfan/playfan-flow.jpg'
     ],
     technologies: [
       'devicon-javascript-plain',
@@ -61,7 +61,7 @@ export const projects: projectI[] = [
     description: 'Red social venezolana, LIFE + WORK son los dos perfiles con los que cuenta la app. La primera es ideal para el entretenimiento, en WORK los usuarios disponen de herramientas adicionales con las que podrán crear contenido interesante para los portales, crear proyectos personales o empresariales en la búsqueda de dar a conocer productos, ideas y servicios.',
     color: '#4581f9',
     images: [
-      '/assets/images/portfolio/oppous-logo.png'
+      './assets/images/portfolio/oppous-logo.png'
     ],
     technologies: [
       'devicon-javascript-plain',
